8221332b0e5ed44b58f1c649256cdb6e8ff6d98d
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-04-12 17:33:29 +0300

    Maintainance, Resgister class, State enum class and 1/3 of the Booking class

---
+++ src/application/Booking.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Date;
+
+public class Booking {
+
+	private int bookingId;
+	private int roomId;
+	private int guestId;
+	private Date startDate;
+	private Date endDate;
+	private double totalPrice;
+	private Boolean isReady;
+	private State state;
+	
+}
---
+++ src/application/Register.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Date;
+import java.util.List;
+
+public class Register {
+	
+	private List<Room> availableRooms;
+	
+	public List<Room> getAvailableRooms() {
+		return availableRooms;
+	}
+
+	public void setAvailableRooms(List<Room> availableRooms) {
+		this.availableRooms = availableRooms;
+	}
+
+	public List<Room> enquire(Date startDate, Date endDate, RoomType roomType) {
+		
+		return List<Room>;
+	}
+	
+
+}
